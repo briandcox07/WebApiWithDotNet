f03cf678115e54bb3020de859697b9ab0d3f15fe
briandcox07 <brian_d_cox@progressive.com>
2024-11-12 10:00:15 -0500

    sync code

---
+++ src/SoftwareSolution/SoftwareApi/Catalog/Models.cs
@@ -0,0 +1,20 @@
+﻿namespace Software.Api.Catalog;
+
+public record CatalogCreateModel
+{
+    public string Name { get; init; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+}
+
+public record CatalogItemResponseModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public Guid VendorId { get; set; }
+
+    public DateTimeOffset AddedToCatalog { get; set; }
+    public string AddedBy { get; set; } = string.Empty;
+
+}
---
+++ src/SoftwareSolution/SoftwareApi/Configuration/DefaultJsonOptions.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Software.Api.Configuration;
+public static class DefaultJsonOptions
+{
+    public static void Configure(JsonOptions options)
+    {
+        options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    }
+}
